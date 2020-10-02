e366f31a145dcf148d353684e38b23bf831839d1
Ivan Tilikin <ivantilikin@Ivans-MacBook-Pro.local>
2020-10-02 16:47:31 +0300

    Build

--- build/precache-manifest.fb57a84b7f0d884bffe77df32004d053.js
+++ build/precache-manifest.fb57a84b7f0d884bffe77df32004d053.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "e95d4986339f94723be4fe5047744723",
-    "url": "/static/media/390FD6_0_0.e95d4986.woff2"
+    "revision": "fef932b63ff570461e2f",
+    "url": "/static/css/main.232b6b14.chunk.css"
   },
   {
-    "revision": "b28a99083f44c54618af",
-    "url": "/static/css/main.0a108c9f.chunk.css"
+    "revision": "fef932b63ff570461e2f",
+    "url": "/static/js/main.8368123c.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -48,36 +48,56 @@ self.__precacheManifest = [
     "url": "/static/media/c.2bd3d74d.svg"
   },
   {
-    "revision": "3515f27ef128c3cbc6e0b44b2dd68964",
-    "url": "/static/media/NocturneSerif-Light.3515f27e.woff2"
-  },
-  {
-    "revision": "e5e03e6deca5cd3954734f9c1696ed2d",
-    "url": "/static/media/390FD6_3_0.e5e03e6d.eot"
-  },
-  {
-    "revision": "9ef3466cddccdedf805c5a54e0a098ea",
-    "url": "/static/media/NocturneSerif-Light.9ef3466c.woff"
-  },
-  {
-    "revision": "b28a99083f44c54618af",
-    "url": "/static/js/main.7ead077a.chunk.js"
+    "revision": "e95d4986339f94723be4fe5047744723",
+    "url": "/static/media/390FD6_0_0.e95d4986.woff2"
   },
   {
     "revision": "9a84eddf8c11602ec5220d798257b153",
     "url": "/static/media/390FD6_0_0.9a84eddf.eot"
   },
   {
+    "revision": "3515f27ef128c3cbc6e0b44b2dd68964",
+    "url": "/static/media/NocturneSerif-Light.3515f27e.woff2"
+  },
+  {
+    "revision": "4478beff6552736d8a7e8131f727b791",
+    "url": "/static/media/390FD6_0_0.4478beff.woff"
+  },
+  {
+    "revision": "9ef3466cddccdedf805c5a54e0a098ea",
+    "url": "/static/media/NocturneSerif-Light.9ef3466c.woff"
+  },
+  {
+    "revision": "a37d460b985283f2bcff1efe68e17eaa",
+    "url": "/static/media/390FD6_0_0.a37d460b.ttf"
+  },
+  {
+    "revision": "e5e03e6deca5cd3954734f9c1696ed2d",
+    "url": "/static/media/390FD6_3_0.e5e03e6d.eot"
+  },
+  {
     "revision": "3f2485f2ee098466eeb1bc042455796d",
     "url": "/static/media/390FD6_3_0.3f2485f2.woff2"
+  },
+  {
+    "revision": "7c9712413c163a6f968d4d60b2e25b21",
+    "url": "/static/media/390FD6_3_0.7c971241.woff"
   },
   {
     "revision": "4612750acd225a28c3baec12fc39f27b",
     "url": "/static/media/390FD6_2_0.4612750a.woff2"
   },
   {
+    "revision": "c37d4466fe0ac9a6c658633761014120",
+    "url": "/static/media/390FD6_3_0.c37d4466.ttf"
+  },
+  {
     "revision": "a788f6ecc2bf373c486c0db59a6e1f4a",
     "url": "/static/media/390FD6_2_0.a788f6ec.eot"
+  },
+  {
+    "revision": "d93f6cd9c98df2a20b45bb6030c69ab0",
+    "url": "/static/media/390FD6_2_0.d93f6cd9.woff"
   },
   {
     "revision": "548142f9efdb76a88157f82b687e51a9",
@@ -88,39 +108,19 @@ self.__precacheManifest = [
     "url": "/static/media/390FD6_1_0.3e472312.woff2"
   },
   {
-    "revision": "4478beff6552736d8a7e8131f727b791",
-    "url": "/static/media/390FD6_0_0.4478beff.woff"
-  },
-  {
-    "revision": "d93f6cd9c98df2a20b45bb6030c69ab0",
-    "url": "/static/media/390FD6_2_0.d93f6cd9.woff"
-  },
-  {
-    "revision": "7c9712413c163a6f968d4d60b2e25b21",
-    "url": "/static/media/390FD6_3_0.7c971241.woff"
+    "revision": "9049d2b2458cbbd15f50d95a3522fe8b",
+    "url": "/static/media/390FD6_2_0.9049d2b2.ttf"
   },
   {
     "revision": "390cb624f5ae8e437441ed25dcc51f83",
     "url": "/static/media/390FD6_1_0.390cb624.woff"
   },
   {
-    "revision": "a37d460b985283f2bcff1efe68e17eaa",
-    "url": "/static/media/390FD6_0_0.a37d460b.ttf"
-  },
-  {
-    "revision": "9049d2b2458cbbd15f50d95a3522fe8b",
-    "url": "/static/media/390FD6_2_0.9049d2b2.ttf"
-  },
-  {
-    "revision": "c37d4466fe0ac9a6c658633761014120",
-    "url": "/static/media/390FD6_3_0.c37d4466.ttf"
-  },
-  {
     "revision": "60a20ab38f68edb071a69fe90a96ca06",
     "url": "/static/media/390FD6_1_0.60a20ab3.ttf"
   },
   {
-    "revision": "7285c974cffe2e5d7b842686bb6fbf43",
+    "revision": "469cbb70bf8d1cda0cff984c46cc22d3",
     "url": "/index.html"
   }
 ];
